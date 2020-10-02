2951175971198939e82b43c340a1b6e87f879e27
Hardeyemi7 <Hardeyemi7@users.noreply.github.com>
2020-10-02 21:37:43 +0100

    Landing page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/pages/LandingPage/Home';
+import SignUp from './components/pages/SignUp/SignUp';
+import SignIn from './components/pages/SignIn/SignIn';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path= '/' exact component={Home}/>
+        <Route path='/sign-up' component={SignUp} />
+        <Route path='/sign-in' component={SignIn} />
+      </Switch>
+    </Router>
   );
 }
 
